5fead0708a546c2da8a0f7fccf70183d91c9237b
Minjae Isaac Kwon <minjae.isaac.kwon@gmail.com>
2022-09-07 10:33:25 +0900

    Add detail of definition of QPSignalGaussian It generated with Sx Sy STheta

--- src/QPSignalGaussian.cpp
+++ src/QPSignalGaussian.cpp
@@ -3,12 +3,11 @@
 #include "QPMath.hpp"
 #include "QPVector.hpp"
 
-QPSignalGaussian::QPSignalGaussian(const QPVector& x0, const double& amplitude, const QPMatrix& S): QPSignalBase(amplitude), fx0(x0),fS(S){
-
-}
+QPSignalGaussian::QPSignalGaussian(const QPVector& x0, const double& amplitude, const QPMatrix& S): QPSignalBase(amplitude), fx0(x0),fS(S){;}
 
 QPSignalGaussian::QPSignalGaussian(const double& x0, const double& y0, const double& amplitude, const double& sx, const double& sy, const double& srad): QPSignalBase(amplitude){
-
+    fx0 = QPVector(x0,y0);
+    fS  = QPMatrix(2,2,4,sx,0,0,sy)*QPMath::RotationMatrix(srad);
 }
 
 double QPSignalGaussian::GetPDF(const double& x, const double& y){
